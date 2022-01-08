3102b173143405244462d341ad6283c8ea9e2bc2
Robbin Schepers <robbin@deschepers.nl>
2022-01-08 23:27:02 +0100

    feat: Add options to customise the execution functions (get/do) to your own liking

--- src/graphton.js
+++ src/graphton.js
@@ -7,8 +7,12 @@ async function main() {
         .command('generate')
         .argument('<schemaUri>', 'URL to GraphQL endpoint or path to introspection json.')
         .option('-o, --outputFile <path>', 'Path to the generated js/ts file', './src/graphton.generated.ts')
-        .option('-q, --exportQueryFactoryAs <name>', 'How you want to import your queries instance.', 'Query')
-        .option('-m, --exportMutationFactoryAs <name>', 'How you want to import your mutations instance.', 'Mutation')
+        .option('-q, --exportQueryFactoryAs <name>', 'How you want to import your query factory.', 'Query')
+        .option('-m, --exportMutationFactoryAs <name>', 'How you want to import your mutation factory.', 'Mutation')
+        .option('-s, --exportSubscriptionFactoryAs [name]', 'If you want a subscription factory, if no name provided "Subscription" will be used.', false)
+        .option('-Q, --queryFunction <name>', 'The name of the function that posts the query.', 'get')
+        .option('-M, --mutateFunction <name>', 'The name of the function that posts the mutation.', 'do')
+        .option('-S, --subscribeFunction <name>', 'The name of the function that posts the subscription.', 'subscribe')
         .action((...params) => (new GenerateCommand()).generate(...params));
     await program.parseAsync(process.argv);
 }
